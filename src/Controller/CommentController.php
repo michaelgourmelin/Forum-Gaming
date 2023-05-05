ef2438f15b318af89178c56db613055a71f7e256
Michael Gourmelin <michael.gourmelin@oclock.school>
2023-05-05 10:23:27 +0200

    modif

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Theme;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/comment', name: 'comment_')]
 
@@ -20,12 +24,44 @@ class CommentController extends AbstractController
         CommentRepository $commentRepository,
         Request $request
     ): Response {
-       
+
 
         $comment = $theme->getComments();
 
         return $this->render('comment/list.html.twig', compact('theme', 'comment'));
-       
+    }
+
+    #[Route('/ajoutcom', name: 'commentaire')]
+
+    public function addcom(
+        Request $request,
+        EntityManagerInterface $em,
+        SluggerInterface $slugger
+    ): Response {
+
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $theme = new Theme();
+        $slug = $slugger->slug($theme->getName());
+        $theme->setSlug($slug);
+        $comment = new Comment();
+        $comment->setUsers($this->getUser());
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+          
+            $em->persist($comment);
+            $em->flush();
+            $this->addFlash('success', 'Commentaire ajouté');
+
+            return $this->redirectToRoute('index_');
+        }
+        return $this->render('comment/addcom.html.twig',[
+
+            'form' =>$form->createView()
+    
+        ]);
     }
 }
-
---
+++ src/Form/CommentFormType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class CommentFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('commentaire', options:[
+        
+             
+            'constraints' => [
+            new NotBlank([
+                'message' => 'Merci de saisir un thème',
+
+            ]),
+            new Length([
+                'min' => 5,
+                'minMessage' => 'Votre texte ne peut pas faire moins de {{ limit }} caractères',
+                // max length allowed by Symfony for security reasons
+                'max' => 600,
+                'maxMessage' => 'Votre texte ne peut pas faire plus de {{ limit }} caractères',
+                
+            ])
+        ],
+    ]);
+    
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
