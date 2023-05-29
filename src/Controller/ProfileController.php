ee6e8e9416a9500d3970167c411dfcc047546831
Michael Gourmelin <michael.gourmelin@oclock.school>
2023-05-29 16:30:34 +0200

    profil

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Repository\UsersRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,18 +15,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfileController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index()
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'Profil de l\'utilisateur',
-        ]);
+        return $this->render('profile/index.html.twig');
     }
 
-    #[Route('/commandes', name: 'orders')]
-    public function orders(): Response
-    {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'Commandes de l\'utilisateur',
-        ]);
+
+
+    
+    #[Route('/modifercom/{slug}', name: 'modifier')]
+    public function updateProduct(Request $request, EntityManagerInterface $entityManager, $id)
+{
+    $product = $entityManager->getRepository(Product::class)->find($id);
+
+    // Check if the entity exists
+    if (!$product) {
+        throw $this->createNotFoundException('Product not found');
     }
+
+    // Update the entity properties based on the request data
+    $product->setName($request->request->get('name'));
+    $product->setPrice($request->request->get('price'));
+    // ... Update other properties as needed
+
+    // Persist the changes to the database
+    $entityManager->flush();
+
+    // Optionally, redirect to another page or return a response
+    return $this->redirectToRoute('product_show', ['id' => $product->getId()]);
+}
+
 }
