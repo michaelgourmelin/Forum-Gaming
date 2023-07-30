bd5e541d3f1fabcf3c303b81a6cf642a8fbf33eb
Michael Gourmelin <michael.gourmelin@oclock.school>
2023-07-30 14:18:29 +0200

    admin

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -30,5 +33,13 @@ class CommentCrudController extends AbstractCrudController
 
 
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            // ...
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT);
     }
 }
--- src/Controller/Admin/ThemeCrudController.php
+++ src/Controller/Admin/ThemeCrudController.php
@@ -3,11 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Theme;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ThemeCrudController extends AbstractCrudController
 {
@@ -16,17 +21,47 @@ class ThemeCrudController extends AbstractCrudController
         return Theme::class;
     }
 
-
     public function configureFields(string $pageName): iterable
     {
         return [
 
-            yield  IdField::new('id'),
+            yield IdField::new('id'),
             yield  AssociationField::new('categories')->autocomplete(),
             yield  TextField::new('name'),
             yield  DateTimeField::new('created_at'),
-
-
+            yield  AssociationField::new('users')->autocomplete(),
         ];
     }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            // ...
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT);
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityPermission('ROLE_ADMIN'); // Adjust the permission as needed
+    }
+
+    public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        // Check if the user has the required ROLE_ADMIN role
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException();
+        }
+
+        // Handle the deletion of related entities (themes and comments) manually
+        $themes = $entityInstance->getComments();
+        foreach ($themes as $theme) {
+            $entityManager->remove($theme);
+        }
+
+        $entityManager->remove($entityInstance);
+        $entityManager->flush();
+    }
+
 }
